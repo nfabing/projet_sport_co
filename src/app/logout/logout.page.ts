66925b41d880362f2e676af9118d70c471432c0b
Simon K <simonkiffer@gmail.com>
2020-02-27 17:55:04 +0100

    Dernière version de la page Logout

--- src/app/logout/logout.page.ts
+++ src/app/logout/logout.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import {ToastController} from '@ionic/angular';
 import {Storage} from '@ionic/storage';
 
 @Component({
@@ -9,15 +10,24 @@ import {Storage} from '@ionic/storage';
 })
 export class LogoutPage implements OnInit {
 
-  constructor(public storage: Storage, public router: Router) { }
+  constructor(public storage: Storage, public router: Router, private toastController: ToastController) {
+  }
 
   ngOnInit() {
   }
-
+  async showToast(msg): Promise<void> {
+    const toast = await this.toastController.create({
+      message: msg,
+      color: 'dark',
+      duration: 2000
+    });
+    await toast.present();
+  }
 
   async ionViewWillEnter() {
     await this.storage.set('id_user', '0');
     await this.storage.set('id_club', '0');
+    this.showToast('Déconnecté');
     await this.router.navigate(['login-register']);
   }
 }
